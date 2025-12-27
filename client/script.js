f361fdfce3bf6a9f309796ae15404726176e42bb
Samir Ahmad <sam.mastoor@hotmail.com>
2025-12-27 13:15:04 +0100

    Uppdate server

---
+++ client/script.js
@@ -0,0 +1,33 @@
+const url = "http://localhost:3000/res";
+
+window.addEventListener("load", fetchData);
+
+function fetchData() {
+  fetch(url)
+    .then((result) => result.json())
+    .then((res) => {
+      if (res.length > 0) {
+        let html =
+          '<ul class="w-3/4 my-3 mx-auto flex flex-wrap gap-2 justify-center">';
+        res.forEach((res) => {
+          html += `<li class="bg-${res.color}-200 basis-1/4 text-${res.color}-900 p-2 rounded-md border-2 border-${res.color}-400 flex flex-col justify-between">
+          <h3>${res.firstName} ${res.lastName}</h3>
+          <p>Användarnamn: ${res.username}</p>
+          <div>
+            <button class="border border-${res.color}-300 hover:bg-white/100 rounded-md bg-white/50 p-1 text-sm mt-2">
+              Ändra
+            </button>
+            <button class="border border-${res.color}-300 hover:bg-white/100 rounded-md bg-white/50 p-1 text-sm mt-2">
+              Ta bort
+            </button>
+          </div>
+        </li>`;
+        });
+        html += "</ul>";
+
+        const listContainer = document.getElementById("listContainer");
+        listContainer.innerHTML = "";
+        listContainer.insertAdjacentHTML("beforeend", html);
+      }
+    });
+}
---
+++ server/server.js
@@ -0,0 +1,30 @@
+const sqlite = require("sqlite3").verbose();
+const db = new sqlite.Database("./gik339.db");
+
+const express = require("express");
+const server = express();
+
+server
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+
+    next();
+  });
+
+server.listen(3000, () => {
+  console.log("Server is running on port http://localhost:3000");
+});
+
+server.get("/res", (req, res) => {
+  const sql = "SELECT * FROM res";
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      res.status(500).send({ error: err.message });
+    } else res.send(rows);
+  });
+});
